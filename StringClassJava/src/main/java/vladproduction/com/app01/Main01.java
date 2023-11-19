28c3bea988a1d683f55febd539e3cd95ab1a584e
vladproduction <vladbogdantsev@gmail.com>
2023-11-19 17:51:38 +0200

    StringClassJava add

---
+++ StringClassJava/src/main/java/vladproduction/com/app01/Main01.java
@@ -0,0 +1,27 @@
+package vladproduction.com.app01;
+
+public class Main01 {
+    public static void main(String[] args) {
+        String message = "Hello, World!";
+        System.out.println(message);
+
+// string concatenation
+        String greeting = "Hello";
+        String name = "John";
+        String fullGreeting = greeting + ", " + name + "!";
+        System.out.println(fullGreeting);
+
+// string length
+        int length = message.length();
+        System.out.println("Length of the message: " + length);
+
+// substring extraction
+        String subStr = message.substring(7);
+        System.out.println("Substring: " + subStr);
+
+// searching in a string
+        boolean containsWorld = message.contains("World");
+        System.out.println("Contains 'World': " + containsWorld);
+
+    }
+}
