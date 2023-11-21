c5c06838571b44abdc86dff878d9731b9ec6c9a0
vladproduction <vladbogdantsev@gmail.com>
2023-11-21 15:43:03 +0200

    TimeZone

---
+++ Date-Calendar/src/main/java/vladproduction/com/zone_time/Time_ZoneId.java
@@ -0,0 +1,23 @@
+package vladproduction.com.zone_time;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Set;
+
+public class Time_ZoneId {
+    public static void main(String[] args) {
+        LocalDate localDate = LocalDate.now();
+        LocalTime localTime = LocalTime.now();
+        ZoneId zoneId = ZoneId.of("Europe/Kiev");
+        ZonedDateTime zdt = ZonedDateTime.of(localDate, localTime,zoneId);
+        System.out.println(zdt);
+
+        Set<String> allZoneIds = ZoneId.getAvailableZoneIds();
+        System.out.println(allZoneIds);
+
+
+    }
+
+}
