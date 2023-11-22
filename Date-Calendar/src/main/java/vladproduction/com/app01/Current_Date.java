d5f2aa3a033d68283c0a3ad1c84ebfc179b1bc01
vladproduction <vladbogdantsev@gmail.com>
2023-11-22 10:15:31 +0200

    Date-Calendar app01 add

---
+++ Date-Calendar/src/main/java/vladproduction/com/app01/Current_Date.java
@@ -0,0 +1,11 @@
+package Date;
+
+import java.util.Date;
+
+public class Current_Date {
+
+    public static void main(String[] args) {
+        System.out.println("Current date: ");
+        System.out.println(new Date());
+    }
+}
