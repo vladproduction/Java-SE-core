9574788bfc4aef49a1a8d4a98ff8b6d5ada9c895
vladproduction <vladbogdantsev@gmail.com>
2023-11-23 21:29:15 +0200

    Classes-Objects-Methods add

---
+++ Classes-Objects-Methods/src/main/java/vladproduction/com/objects_methods/Person.java
@@ -0,0 +1,14 @@
+package vladproduction.com.objects_methods;
+
+public class Person{
+	
+	public void printData(String firstName, String lastName, int age){
+		System.out.println("You have " + age + " years old");
+		System.out.println("Your name is: " + firstName + " " + lastName);
+	}
+
+	public String getName(String firstName, String lastName){
+		String completeName = "Your name is: " + firstName + " " + lastName;
+		return completeName;
+	}
+}
